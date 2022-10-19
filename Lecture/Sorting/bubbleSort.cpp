bf3851072562b3c09c5b2dc3cad2c419af10d6da
Shaquille <shaquilledputra@gmail.com>
2022-10-19 10:48:25 +0700

    function in bubble  sort

--- Lecture/Sorting/bubbleSort.cpp
+++ Lecture/Sorting/bubbleSort.cpp
@@ -3,6 +3,27 @@
 #include <algorithm>
 #define N 1000
 
+void Swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void Bubble(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                Swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
 int main()
 {
     int angka[N + 5];
@@ -11,7 +32,7 @@ int main()
         angka[i] = (rand() % 100) + 1;
     }
 
-    std::sort(angka, angka + 10);
+//    std::sort(angka, angka + 10);
     for(int i = 0; i < 10; i++)
     {
         printf("%d\n", angka[i]);
