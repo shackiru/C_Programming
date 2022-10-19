f0f60730e9c97a1861a41d7a6454c7f4d7777a3b
Shaquille <shaquilledputra@gmail.com>
2022-10-19 09:16:48 +0700

    create function

--- Lecture/Sorting/countSort.cpp
+++ Lecture/Sorting/countSort.cpp
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+void Counting(int arr[], int n)
+{
+	
+}
+
 int main()
 {
-    srand(time(0));
-    printf("%d\n", rand() % 10 + 1);
+//    srand(time(0));
+//    printf("%d\n", rand() % 101 + 100);
+
+	int num[1005];
+	for(int i = 0; i < 1000; i++)
+	{
+		num[i] = (rand() % 100) + 1;
+		printf("%d ", num[i]);
+	}
+	printf("\n");
 
     return 0;
 }
