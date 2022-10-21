14761b56cf00ca68fb1d35285e6e3c8dd002d5e1
Shaquille <shaquilledputra@gmail.com>
2022-10-21 14:03:32 +0700

    add data

--- LAB/File Processing/FileProcessing.cpp
+++ LAB/File Processing/FileProcessing.cpp
@@ -5,6 +5,7 @@
 char name[200][30];
 char score[200];
 char major[200][50];
+int index = 0;
 
 void displayAll()
 {
@@ -22,28 +23,55 @@ void addData()
 	char inputName[30];
 	int inputScore;
 	char inputMajor[50];
-	
+    
 	do
 	{
-		printf("Enter Your Name[1-30 Characters]:");
+		printf("Enter Your Name[1-50 Characters]:");
 		scanf("%[^\n]", inputName);
 		getchar();
 	}
-	while(strlen(inputName) < 1 || strlen(inputName) > 30);
+	while(strlen(inputName) < 1 || strlen(inputName) > 50);
+	
+	do
+	{
+		printf("Enter Your Score [1-100]:");
+		scanf("%d", &inputScore);
+		getchar();
+	}
+	while(inputScore < 1 || inputScore > 100);
+	
+	do
+	{
+		printf("Enter Your Major[1-50 Characters]:");
+		scanf("%[^\n]", inputMajor);
+		getchar();
+	}
+	while(strlen(inputMajor) < 1 || strlen(inputMajor) > 50);
+	
+	strcpy(name[index], inputName);
+	score[index] = inputScore;
+	strcpy(major[index], inputMajor);
+	index++;
+	
 }
 
 void mainMenu()
 {
-	int input;
+	int input = -1;
+	
+	
 	do
 	{
-		printf("Main Menu!\n");
+		printf("Main Menu\n");
 		printf("1. View Data\n");
 		printf("2. Add Data\n");
 		printf("3. Update Data\n");
 		printf("4. Delete Data\n");
 		printf("5. Exit\n");
 		
+		scanf("%d", &input);
+		getchar();
+			
 		switch(input)
 		{
 			case 1:
@@ -67,14 +95,32 @@ void mainMenu()
 			{
 				break;	
 			}
+			
+			case 5:
+			{
+				break;
+			}
 		}
 	}
-	while(input < 1 || input > 5);
+	while(true);
+}
+
+void exitProgram()
+{
+	FILE *fp = fopen("Result.csv", "w");
+	
+	int i = 0;
+	while(i < index)
+	{
+		fprintf(fp, "%s, %d, %s\n", name[i], score[i], major[i]);
+        i++;
+	}
+    fclose(fp);
+    printf("Thank you for using this program\n");
 }
 
 int main()
 {
-    int index = 0;
 
     FILE *f = fopen("Score.txt", "r");
 
@@ -84,14 +130,13 @@ int main()
     }
     else
     {
-        printf("File Exist\n");
         while(!feof(f))
         {
             fscanf(f, "%[^#]#%d#%[^\n]\n", name[index], &score[index], major[index]);
             index++;
         }
-        displayAll();
     }
+    
 	mainMenu();
     fclose(f);
 
