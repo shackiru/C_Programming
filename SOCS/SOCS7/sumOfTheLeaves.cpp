6acc49e5f3220b140987d6fbf15bc19c66dcbf9a
Shaquille <shaquilledputra@gmail.com>
2022-10-22 17:45:34 +0700

    badprank.cpp

--- SOCS/SOCS7/sumOfTheLeaves.cpp
+++ SOCS/SOCS7/sumOfTheLeaves.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void sumOfLeaves(int* array, int size, int sum, int index)
+void sumOfLeaves(int* array, int size, int index, int sum)
 {
     if(index == size)
     {
@@ -23,24 +23,25 @@ void sumOfLeaves(int* array, int size, int sum, int index)
     }
 
     sumOfLeaves(array, size, 2 * index, sum + array[2 * index]);
+    
     sumOfLeaves(array, size, (2 * index) + 1, sum + array[(2 * index) + 1]);
 }
 
 void SumResult()
 {
     int testCase;
-    int amountCase;
 
     scanf("%d", &testCase);
     getchar();
 
     for(int tc = 0; tc < testCase; tc++)
-    {
+    {    
+		int amountCase;
         scanf("%d", &amountCase);
         getchar();
         int array[amountCase + 1];
 
-        for(int i = 0; i < amountCase; i++)
+        for(int i = 1; i <= amountCase; i++)
         {
             scanf("%d", &array[i]);
         }
