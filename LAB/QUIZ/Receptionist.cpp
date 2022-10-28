5860d54ed096922189a6082b707ee154cc261cf9
Shaquille <shaquilledputra@gmail.com>
2022-10-28 13:17:54 +0700

    quiz lab

---
+++ LAB/QUIZ/Receptionist.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct Visitor
+{
+    char id[7];
+    char name[105];
+    int age[105];
+
+};
+
+Visitor visitorArr[205];
+
+void readFile()
+{
+
+}
+
+void mainMenu()
+{
+
+}
+
+void addData()
+{
+
+}
+
+void updateData()
+{
+
+}
+
+void deleteData()
+{
+
+}
+
+int main()
+{
+    srand(time(0));
+    readFile();
+    mainMenu();
+
+    return 0;
+}
