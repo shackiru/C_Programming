57211a9638615ecebfe208e768aa3f816d8d21e4
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-11-02 09:42:07 +0700

    function

--- Lecture/Sorting/QuickSort.cpp
+++ Lecture/Sorting/QuickSort.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int partition(int *arr, int low, int high)
 {
-    
+    int pivot = arr[low];
+    int index = low + 1;
+
+    for(int i = low + 1; i <= high; i++)
+    {
+        if(arr[i] < pivot)
+        {
+            swap(&arr[index], &arr[i]);
+            index++;        }
+    }
+    swap(&arr[low], &arr[index--]);
+    return index--;
 }
 
 void quickSort(int arr[], int low, int high)
@@ -18,7 +37,14 @@ void quickSort(int arr[], int low, int high)
 
 int main()
 {
+    int arr[1005];
 
+    for(int i = 0; i < 1000; i++)
+    {
+        arr[i] = rand() % 1000 + 1;
+    }
+
+    quickSort(arr, 0, 999);
 
     return 0;
 }
