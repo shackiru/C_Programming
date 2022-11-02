b2bda8a161a008557640cebf90b629e08ddb3a93
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-11-02 11:59:02 +0700

    binary search complete

--- Lecture/Searching/BinarySearch.cpp
+++ Lecture/Searching/BinarySearch.cpp
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
-int binarySearch(int *arr, int n, int )
+int binarySearch(int *arr, int n, int find)
 {
+    int low = 0;
+    int high = n - 1;
+    int mid;
 
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+        if(arr[mid] == find)
+        {
+            return mid;
+        }
+        else if(find < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
 }
 
 int main()
 {
-    
+
 
     return 0;
 }
