3f478ca8d8a2c4bcb104f5d5b47da8f872c5013d
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-11-02 08:45:07 +0700

    test

--- Lecture/General/FirstLesson.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	//Sedang Belajar C Mohon Bantuannya Ya!
-	printf("Hello World\n\n");
-	printf("I am learning C");
-	return 0;
-}
--- Review/SOCS1/HelloBinusan.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello Binusian!\n");
-
-    return 0;
-}
