3cffe626d915a9f140882b9be0ab61c30d456dc0
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-11-08 10:04:16 +0700

    abdi

--- SOCS/SOCS9/codeKoAbdiD.cpp
+++ SOCS/SOCS9/codeKoAbdiD.cpp
@@ -4,57 +4,43 @@
 int main(){
 	int tc;
 	int total[30];
-	int result[10] = {0};
+	int jawab[10]={0};
 	int arr[30];
-	int max = 0;
-	int min = 20000001;
-	int idxMax = -1;
-	int idxMin = -1;
+	int max=0;
+	int min=20000001;
+	int idxMax=-1;
+	int idxMin=-1;
 	
-	for(int i = 0 ; i < 30; i++)
-	{
-		arr[i] =0 ;
+	for(int i=0; i<30; i++){
+		arr[i]=0;
 	}
 	
 	scanf("%d", &tc);
-	for(int i = 0; i < tc; i++)
-	{
-		for(int j = 0; j < 5; j++)
-		{
-			result[j]=0;
+	for(int i=0; i<tc; i++){
+		for(int j=0; j<5; j++){
+			jawab[j]=0;
 		}
-		for(int j = 0; j < 30; j++)
-		{
-			arr[j] = 0;
-			total[j] = 0;
+		for(int j=0; j<30; j++){
+			arr[j]=0;
+			total[j]=0;
 		}
-
-		max = 0;
-		min = 20000001;
-		idxMax = -1;
-		idxMin = -1;
-
-		for(int j = 0; j < 25; j++)
-		{
+		max=0;
+		min=20000001;
+		idxMax=-1;
+		idxMin=-1;
+		for(int j=0; j<25; j++){
 			scanf("%d", &total[j]);
 			arr[j]++;
-
-			if(max < total[j])
-			{
-				max = total[j];
-				idxMax = j;
+			if(max<total[j]){
+				max=total[j];
+				idxMax=j;
 			}
-
-			if(min > total[j])
-			{
-				min = total[j];
-				idxMin = j;
+			if(min>total[j]){
+				min=total[j];
+				idxMin=j;
 			}
-
-			for(int k = 0; k < j; k++)
-			{
-				if(total[j] == total[k])
-				{
+			for(int k=0; k<j; k++){
+				if(total[j]==total[k]){
 					arr[j]--;
 					arr[k]++;
 					break;
@@ -62,93 +48,72 @@ int main(){
 			}
 		}
 		printf("Case #%d:", i+1);
-		if(arr[idxMax] == 1)
-		{
-			result[4] = max / 2;
+		if(arr[idxMax]==1){
+			jawab[4]=max/2;
 		}
-		if(arr[idxMin] == 1)
-		{
-			result[0] = min / 2;
+		if(arr[idxMin]==1){
+			jawab[0]=min/2;
 		}
-		if(arr[idxMax] == 4)
-		{
-			result[4] = max / 2;
-			result[3] = max / 2;
+		if(arr[idxMax]==4){
+			jawab[4]=max/2;
+			jawab[3]=max/2;
 		}
-		if(arr[idxMin] == 4)
-		{
-			result[0] = min / 2;
-			result[1] = min / 2;
+		if(arr[idxMin]==4){
+			jawab[0]=min/2;
+			jawab[1]=min/2;x
 		}
-		if(arr[idxMax] == 9)
-		{
-			result[4] = max / 2;
-			result[3] = max / 2;
-			result[2] = max / 2;
+		if(arr[idxMax]==9){
+			jawab[4]=max/2;
+			jawab[3]=max/2;
+			jawab[2]=max/2;
 		}
-		if(arr[idxMin] == 9)
-		{
-			result[0] = min / 2;
-			result[1] = min / 2;
-			result[2] = min / 2;
+		if(arr[idxMin]==9){
+			jawab[0]=min/2;
+			jawab[1]=min/2;
+			jawab[2]=min/2;
 		}
-		if(arr[idxMax] == 16)
-		{
-			result[4] = max/ 2;
-			result[3] = max/ 2;
-			result[2] = max/ 2;
-			result[1] = max/ 2;
+		if(arr[idxMax]==16){
+			jawab[4]=max/2;
+			jawab[3]=max/2;
+			jawab[2]=max/2;
+			jawab[1]=max/2;
 		}
-		if(arr[idxMin] == 16)
-		{
-			result[0] = min / 2;
-			result[1] = min / 2;
-			result[2] = min / 2;
-			result[3] = min / 2;
+		if(arr[idxMin]==16){
+			jawab[0]=min/2;
+			jawab[1]=min/2;
+			jawab[2]=min/2;
+			jawab[3]=min/2;
 		}
-		if(arr[idxMin] == 25)
-		{
-			result[0] = min / 2;
-			result[1] = min / 2;
-			result[2] = min / 2;
-			result[3] = min / 2;
-			result[4] = min / 2;
+		if(arr[idxMin]==25){
+			jawab[0]=min/2;
+			jawab[1]=min/2;
+			jawab[2]=min/2;
+			jawab[3]=min/2;
+			jawab[4]=min/2;
 		}		
 		
-		for(int j = 0; j < 25; j++)
-		{
-			if((total[j] % 2 == 0)
-			&&(arr[j] == 1 || 
-			arr[j] == 3 || 
-			arr[j] == 5) 
-			&& total[j] != max 
-			&& total[j] != min)
-			{
-				for(int k=0; k < 5; k++)
-				{
-					if(result[k] == 0)
-					{
-						result[k] = total[j] / 2;
+		for(int j=0; j<25; j++){
+			if((total[j]%2==0)&&(arr[j]==1 || arr[j]==3 || arr[j]==5) && total[j]!=max && total[j]!=min){
+				for(int k=0; k<5; k++){
+					if(jawab[k]==0){
+						jawab[k]=total[j]/2;
 						break;
 					}
 				}
 			}
 		}
-		for(int j = 0; j < 5; j++)
-		{
-			for(int k = 0; k < 4 - j; k++)
-			{
-				if(result[k] > result[k+1]){
-					int temp;
-					temp = result[k];
-					result[k] = result[k+1];
-					result[k+1] = temp;
+		for(int j=0; j<5; j++){
+			for(int k=0; k<4-j; k++){
+				if(jawab[k]>jawab[k+1]){
+					int cont;
+					cont=jawab[k];
+					jawab[k]=jawab[k+1];
+					jawab[k+1]=cont;
 				}
 			}
 		}
-		for(int j = 0; j < 5; j++)
-		{
-			printf(" %d", result[j]);
+		for(int j=0; j<5; j++){
+			printf(" %d", jawab[j]);
 		}
 		printf("\n");
 	}			
