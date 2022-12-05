3ef370d21482f3930befc27956a067602b99f9f9
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-12-05 10:45:44 +0700

    lecture

---
+++ Lecture/Data Structure/Session 01/Array2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x[100000];
+    int i, check;
+
+    printf("Jika mau Input, type 1: ");
+    scanf("%d", &check);
+
+    if(check == 1)
+    {
+        scanf("%d", &x[i]);
+        i++;
+    }
+    return 0;
+}
---
+++ Lecture/Data Structure/Session 01/TreeDelete.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10] = {0, 1 ,2 ,3 ,4 ,5, 6, 7, 8, 9,};
+
+    int index = 10;
+    int target = 4;
+
+    for(int i = 0; i < index; i++)
+    {
+        if(arr[i] == target)
+        {
+            for(int j = 0; j < index; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            index --;
+            break;
+        }
+    }
+
+    for(int i = 0; i < index; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
