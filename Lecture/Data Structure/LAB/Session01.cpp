71c88df8f17ff6fe2eff3f688d26443d10710f33
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-12-12 13:48:19 +0700

    lab

--- Lecture/Data Structure/LAB/Session01.cpp
+++ Lecture/Data Structure/LAB/Session01.cpp
@@ -4,13 +4,29 @@ int main()
 {
     int a = 5;
     int *b = &a;
+    int **c = &b;
 
-    printf("Address of a: %d\n", &a);
-    printf("Value of a: %d\n", a);
+    printf("Address of a\t: %d\n", &a);
+    printf("Value of a\t: %d\n", a);
     printf("\n");
-    printf("Address of b: %d\n", &b);
-    printf("Value of b: %d\n", b);
-    printf("Value of *b: %d\n", *b);
+
+    printf("Address of b\t: %d\n", &b);
+    printf("Value of b\t: %d\n", b);
+    printf("Value of *b\t: %d\n", *b);
+    printf("\n");
+
+    printf("Address of c\t: %d\n", &c);
+    printf("Value of c\t: %d\n", c);
+    printf("Value of *c\t: %d\n", *c);
+    printf("Value of **c\t: %d\n", **c);
+    printf("\n");
+
+    int scores[] = {100, 90, 95, 80};
+    int size = sizeof(scores) / sizeof(scores[0]);
+    for(int i = 0; i < size; i++)
+    {
+        printf("Index %d: %d - %d\n", i + 1, &scores[i], scores[i]);
+    }
 
     return 0;
 }
