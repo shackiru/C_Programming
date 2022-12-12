2681e7bd1e382e2c483c0f87a68123005311cc9d
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-12-12 17:23:25 +0700

    dll function

--- Study/LinkedList/DLL.cpp
+++ Study/LinkedList/DLL.cpp
@@ -45,5 +45,127 @@ void create()
     printf("Enter the Designation of the Employee: ");
     scanf("%s", design);
     getchar();
-    
+
+    printf("Enter the Salary of the Employee: ");
+    scanf("%f", &salary);
+    getchar();
+
+    printf("Enter the Phone Number of the Employee: ");
+    scanf("%ld", &phone);
+    getchar();
+
+    temp->SSN = SSN;
+    strcpy(temp->name, name);
+    strcpy(temp->dept, dept);
+    strcpy(temp->design, design);
+    temp->salary = salary;
+    temp->phone = phone;
+
+    size++;
+}
+
+void firstInsert()
+{
+    if(h == NULL)
+    {
+        create();
+        h = temp;
+        temp2 = h;
+    }
+    else
+    {
+        create();
+        temp->next = h;
+        h->prev = temp;
+        h = temp;
+    }
+}
+
+void lastInsert()
+{
+    if(h == NULL)
+    {
+        create();
+        h = temp;
+        temp2 = h;
+    }
+    else
+    {
+        create();
+        temp2->next = temp;
+        temp->prev = temp2;
+        temp2 = temp;
+    }
+}
+
+void displayFirst()
+{
+    temp3 = h;
+    if(temp3 == NULL)
+    {
+        printf("\nList is empty!\n");
+        return;
+    }
+    else
+    {
+        printf("\nLinked List elements from the beginning: \n");
+        while(temp3 != NULL)
+        {
+            printf("SSN\t: %d\n", temp3->SSN);
+            printf("Name\t: %s\n", temp3->name);
+            printf("Department\t: %s\n", temp3->dept);
+            printf("Designation\t: %s\n", temp3->design);
+            printf("Salary\t: %f\n", temp3->salary);
+            printf("Phone Number\t: %ld\n", temp3->phone);
+            temp3 = temp3->next;
+        }
+        printf("Number of Employees\t: %d", size);
+    }
+}
+
+int deleteFirst()
+{
+    struct Node *temp;
+    temp = h;
+    if(temp == NULL)
+    {
+        printf("\nList is empty!\n");
+        return 0;
+    }
+    if(temp->next == NULL)
+    {
+        printf("SSN\t: %d\n", temp3->SSN);
+        printf("Name\t: %s\n", temp3->name);
+        printf("Department\t: %s\n", temp3->dept);
+        printf("Designation\t: %s\n", temp3->design);
+        printf("Salary\t: %f\n", temp3->salary);
+        printf("Phone Number\t: %ld\n", temp3->phone);
+        free(temp);
+        h = NULL;
+    }
+    else
+    {
+        h = h->next;
+        h->prev = NULL;
+        printf("SSN\t: %d\n", temp3->SSN);
+        printf("Name\t: %s\n", temp3->name);
+        printf("Department\t: %s\n", temp3->dept);
+        printf("Designation\t: %s\n", temp3->design);
+        printf("Salary\t: %f\n", temp3->salary);
+        printf("Phone Number\t: %ld\n", temp3->phone);
+        free(temp);
+    }
+    size--;
+    return 0;
+}
+
+int deleteLast()
+{
+    struct Node *temp;
+    temp = h;
+    if(temp == NULL)
+    {
+        printf("\nList is empty!\n");
+        return 0;
+    }
 }
