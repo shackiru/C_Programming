627940a47e41a7dc6d9e0862c2eae6924be8d81d
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-09 13:23:01 +0700

    Linked List

--- Lecture/Data Structure/LAB/Session 02/linkedlist.cpp
+++ Lecture/Data Structure/LAB/Session 02/linkedlist.cpp
@@ -4,10 +4,22 @@
 struct Food
 {
     int price;
-};
+    struct Food * next;
+}*head = NULL, *tail = NULL;
+
+struct Food *createFood(int price)
+{
+    struct Food *food = (struct Food*) malloc (sizeof(struct Food));
+    food->price = price;
+    food->next = NULL;
+}
 
 int main()
 {
+    int arr[100] = {1, 2, 3};
+
+    struct Food * food = createFood(23000);
+    printf("%d", food->price);
 
     return 0;
 }
