06fa0bf05bb688e5bf49d31f68b99ad60baa5732
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-09 08:37:58 +0700

    stack

--- Lecture/Data Structure/Session 03/Stack.cpp
+++ Lecture/Data Structure/Session 03/Stack.cpp
@@ -1,8 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 10000
+
+int stack[MAX];
+int topIdx;
+
+void push(int val)
+{
+    //insert to ARRAY
+    stack[topIdx++] = val;
+
+    //increase topIDX
+}
+
+int pop()
+{
+    int val = stack[topIdx];
+    stack[topIdx] = 0;
+    topIdx--;
+    if(topIdx == -1)
+    {
+        return -1;
+    }
+    return val;
+}
+
+int top()
+{
+    return stack[topIdx];
+}
+
+void printStack()
+{
+    for(int i = 0; i < topIdx; i++)
+    {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
-    
+    topIdx = 0;
+    push(10);
+    push(5);
+    printStack();
+    pop();
+    printStack();
 
     return 0;
 }
