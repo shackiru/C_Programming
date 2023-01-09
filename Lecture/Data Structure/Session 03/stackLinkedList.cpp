9bd737ca2b8c2220ae56d227d8560cb766d26d5c
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-09 08:50:24 +0700

    linkedlist stack

---
+++ Lecture/Data Structure/Session 03/stackLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int val;
+    struct node * next;
+};
+
+struct node * stack = NULL;
+
+void push()
+{
+
+}
+
+struct node * pop()
+{
+
+}
+
+struct node * top()
+{
+    
+}
+
+int main()
+{
+    return 0;
+}
