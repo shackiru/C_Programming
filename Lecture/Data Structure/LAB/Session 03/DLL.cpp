3013300532574b8194926569d3c1492b823da911
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-16 13:46:23 +0700

    DLL

--- Lecture/Data Structure/LAB/Session 03/DLL.cpp
+++ Lecture/Data Structure/LAB/Session 03/DLL.cpp
@@ -32,6 +32,42 @@ void pushHead(Node **head, Node **tail, Node *node)
     }
 }
 
+void pushTail(Node **head, Node **tail, Node *node)
+{
+    if(*head == NULL)
+    {
+        *head = *tail = node;
+    }
+    else
+    (*tail)->next = node;
+    node->prev = *tail;
+    *tail = node;
+}
+
+void pushValue(Node **head, Node **tail, Node *node)
+{
+    if(*head == NULL || strcmp(node->name, (*head)->name) < 0)
+    {
+        pushHead(head, tail, node);
+    }
+    else if(strcmp(node->name, (*tail)->name) >= 0)
+    {
+        pushTail(head, tail, node);
+    }
+    else
+    {
+        Node *curr = *head;
+        while(strcmp(node->name, curr->next->name) > 0)
+        {
+            curr = curr->next;
+        }
+        node->next = curr->next;
+        curr->next->prev = node;
+        node->prev = curr;
+        curr->next = node;
+    }
+}
+
 void view(Node *head)
 {
     Node *curr = head;
@@ -42,12 +78,67 @@ void view(Node *head)
     }
 }
 
+void popHead(Node **head, Node **tail)
+{
+    if(*head == NULL)
+    {
+        return;
+    }
+    else if(*head == *tail)
+    {
+        free(*head);
+        *head = *tail = NULL;
+    }
+    else
+    {
+        Node *temp = *head;
+        *head = (*head)->next;
+        free(temp);
+        temp = NULL;
+        (*head)->prev = NULL;
+
+        *head = (*head)->next;
+        free((*head)->prev);
+        (*head)->prev = NULL;
+    }
+}
+
+void popTail(Node **head, Node **tail)
+{
+    if(*head == NULL)
+    {
+        return;
+    }
+    else if(*head == *tail)
+    {
+        free(*head);
+        *head = *tail = NULL;
+    }
+    else
+    {
+
+    }
+}
+
 int main()
 {
     Node *head = NULL, *tail = NULL;
 
-    pushHead(&head, &tail, createNode("Budi", 20));
-
+    //pushHead(&head, &tail, createNode("Budi", 20));
+    //pushTail(&head, &tail, createNode("Shaq", 19));
+    printf("Before:\n");
+    pushValue(&head, &tail, createNode("Dexter", 20));
+    pushValue(&head, &tail, createNode("Budi", 20));
+    pushValue(&head, &tail, createNode("Jordy", 20));
+    pushValue(&head, &tail, createNode("Shaq", 20));
+    pushValue(&head, &tail, createNode("Fredoy", 20));
     view(head);
+    printf("\n");
+
+    printf("After:\n");
+    popHead(&head, &tail);
+    view(head);
+    printf("\n");
+    
     return 0;
 }
