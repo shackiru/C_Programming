c38ad3f3ddce89db714eebd5bd7a389d91fd445f
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-16 13:06:26 +0700

    DLL Lab

---
+++ Lecture/Data Structure/LAB/Session 03/DLL.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Node
+{
+    char name[100];
+    int age;
+    Node *next, *prev;
+};
+
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+
+    return 0;
+}
