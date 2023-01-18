6369fe63df8fd444242421029e8679422c3a25a6
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-18 15:55:56 +0700

    Mid Square

--- Lecture/Data Structure/Tutor/Session03/midSquare.cpp
+++ Lecture/Data Structure/Tutor/Session03/midSquare.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define SIZE 26
 
 int main()
 {
     int value = 25;
     int key;
     long long int hashed;
-    char buffer[50];
+    char buffer[50] = {};
 
-    sprintf(buffer, "%lld", 25 * 25);
+    sprintf(buffer, "%lld", 99 * 99);
     printf("%s\n", buffer);
 
-    char keyC[25];
+    char keyC[25] = {};
     if(strlen(buffer) % 2 == 1)
     {
         int mid = strlen(buffer) / 2;
@@ -19,10 +21,21 @@ int main()
     }
     else
     {
-
+        int mid = strlen(buffer) / 2 - 1;
+        strncat(keyC, &buffer[mid], 1);
+        strncat(keyC, &buffer[mid + 1], 1);
     }
-    
-    printf("%c", buffer[1]);
+    char *strBuff;
+    printf("%s\n", keyC);
+
+    // strtol solutin
+    //hashed = strtol(keyC, &strBuff, 10);
+
+    sscanf(keyC, "%lld", &hashed);
+    printf("%lld\n", hashed + 1);
+
+    //division
+    hashed = hashed % SIZE;
 
     return 0;
 }
