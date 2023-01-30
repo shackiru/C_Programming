1c70e6b5ef0b5f5ee2ab50a621747afa4924fc6e
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-01-30 10:09:12 +0700

    Case Shift
    Co-authored-by: Joeliardo <Joeliardo@users.noreply.github.com>

---
+++ Lecture/Data Structure/Session 05/teamSession.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void readFile()
+{
+    FILE *f = fopen("r", "CaseShift2.csv");
+    while(!feof)
+    fscanf(r, );
+}
+
+int main()
+{
+
+    return 0;
+}
