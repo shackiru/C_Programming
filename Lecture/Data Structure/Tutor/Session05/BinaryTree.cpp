ac9f6bd5d758f45f7895b0ea46b13403a30725a1
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-02-02 13:53:39 +0700

    Binary Tree

--- Lecture/Data Structure/Tutor/Session05/BinaryTree.cpp
+++ Lecture/Data Structure/Tutor/Session05/BinaryTree.cpp
@@ -2,7 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct tnode
+{
+    int data;
+    struct tnode *left;
+    struct tnode *right;
+    struct tnode *parent;
+}*root = NULL;
+
+struct tnode * newNode(int x)
+{
+    struct tnode *temp = (struct tnode *) malloc (sizeof(struct tnode));
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->parent = NULL;
+
+    return temp;
+}
+
 int main()
 {
-    
+    int x;
+    scanf("%d", &x);
+    if(root == NULL)
+    {
+        root = newNode(x);
+    }
+    printf("%d \n", root->data);
+
+    scanf("%d", &x);
+    root->left = newNode(x);
+    root->left->parent = root;
+    printf("%d", root->left);
+
+    return 0;
+
 }
