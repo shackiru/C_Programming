82293ee35d6b8bdf29339404594e60399d45ee39
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-02-04 00:14:05 +0700

    Linked list and Binary Tree

--- Lecture/Data Structure/Tutor/Session05/BinaryTree.cpp
+++ Lecture/Data Structure/Tutor/Session05/BinaryTree.cpp
@@ -9,6 +9,7 @@ struct tnode
     struct tnode *right;
     struct tnode *parent;
 }*root = NULL;
+int lev = 0;
 
 struct tnode * newNode(int x)
 {
@@ -19,6 +20,51 @@ struct tnode * newNode(int x)
     temp->parent = NULL;
 
     return temp;
+}
+
+int isLeave(struct tnode *curr)
+{
+    if(curr->left == NULL || curr->right == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int rightMostChild(struct tnode * node)
+{
+    struct tnode *iter = node;
+    while(iter->right)
+    {
+        iter = iter->right;
+    }
+    return iter->data;
+}
+
+int leftMostChild(struct tnode * node)
+{
+    struct tnode *iter = root;
+    while(iter->left)
+    {
+        iter = iter->left;
+    }
+    return iter->data;
+}
+
+void getHeight(int curlev, struct tnode *curr)
+{
+    if(curlev > lev)
+    {
+        lev = curlev;
+    }
+    if(curr->left)
+    {
+        getHeight(curlev + 1, curr->left);
+    }
+    if(curr->right)
+    {
+        getHeight(curlev + 1, curr->right);
+    }
 }
 
 int main()
@@ -33,8 +79,18 @@ int main()
 
     scanf("%d", &x);
     root->left = newNode(x);
-    root->left->parent = root;
-    printf("%d", root->left);
+    //root->left->parent = root;
+    printf("%d \n", root->left->data);
+    scanf("%d", &x);
+    root->left = newNode(x);
+
+    scanf("%d", &x);
+    root->right->right = newNode(x);
+    // printf("%d\n", rightMostChild(root));
+    // printf("%d\n", leftMostChild(root));
+
+    getHeight(0, root);
+    printf("%d\n", lev);
 
     return 0;
 
