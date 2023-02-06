685413ecc6a87df402ae3ecfaf70de56d5ac5877
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-02-06 10:03:54 +0700

    Binary Tree

--- Lecture/Data Structure/Session 06/BinaryTree.cpp
+++ Lecture/Data Structure/Session 06/BinaryTree.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct tnode
 {
@@ -7,6 +8,9 @@ struct tnode
     struct tnode *left;
     struct tnode *right;
 };
+
+char s[100] = {0};
+int p = 0;
 
 struct tnode * newNode(char c)
 {
@@ -16,8 +20,86 @@ struct tnode * newNode(char c)
     return temp;
 }
 
+int isOperator(char c)
+{
+    char op[] = "%*-^+/";
+    if(strchr(op, c))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void insert(struct tnode * curr)
+{
+    if(isOperator(s[p]))
+    {
+        p++;
+        curr->left = newNode(s[p]);
+        insert(curr->left);
+
+        p++;
+        curr->right = newNode(s[p]);
+        insert(curr->right);
+    }
+}
+
+void prefix(struct tnode *curr)
+{
+    printf("%c ", curr->c);
+    if(curr->left != NULL)
+    {
+        prefix(curr->left);
+    }
+    if(curr->right != NULL)
+    {
+        prefix(curr->right);
+    }
+}
+
+void postfix(struct tnode *curr)
+{
+    if(curr->left != NULL)
+    {
+        postfix(curr->left);
+    }
+    if(curr->right != NULL)
+    {
+        postfix(curr->right);
+    }
+    printf("%c ", curr->c);
+}
+
+void postfix(struct tnode *curr)
+{
+    if(isOperator(curr->c))
+    {
+        printf("(");
+    }
+    if(curr->left != NULL)
+    {
+        postfix(curr->left);
+    }
+    printf("%c ", curr->c);
+    if(curr->right != NULL)
+    {
+        postfix(curr->right);
+    }
+    if(isOperator(curr->c))
+    {
+        printf(")");
+    }
+}
+
+
 int main()
 {
-    printf("check");
+    scanf("%s", s);
+    struct tnode *root = newNode(s[0]);
+    insert(root);
+    prefix(root);
+    printf("\n");
+    postfix(root);
+    infix(root);
     return 0;
 }
