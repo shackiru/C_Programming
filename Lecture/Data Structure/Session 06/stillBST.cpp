8678c8da76ae10838282c557cf67dd1a7003ca23
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-02-06 10:46:45 +0700

    Learning BST

---
+++ Lecture/Data Structure/Session 06/stillBST.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct tnode
+{
+    int val;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+struct tnode *root = 0;
+
+struct tnode *newNode(int val)
+{
+    struct tnode *temp = (struct tnode *) malloc (sizeof(struct tnode));
+    temp->val = val;
+    temp->left = temp->right = NULL;
+}
+
+void insertNode(struct tnode *curr, int x)
+{
+    if(root == NULL)
+    {
+        root = newNode(x);
+    }
+    else
+    {
+        if(x < curr->val && curr->left == NULL)
+        {   
+        // jika x lebih kecil dari curr val dan curr adalah leaf, insert
+
+        }
+        else if(x > curr->val && curr->right == NULL)
+        {
+        // jika x lebih besar dari curr val dan curr adalah leaf, insert
+
+        }
+        else if(x < curr->val)
+        {
+        // jika x lebih kecil dari curr val, ke kiri
+
+        }
+        else if(x > curr->val)
+        {
+        // jika x lebih besar dari curr val, ke kanan
+
+        }
+    }
+}
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    insertNode(root , x);
+
+    return 0;
+}
